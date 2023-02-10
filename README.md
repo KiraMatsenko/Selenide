889a44f39cf767a2e080267022a6c40b7efb2eb3
Kira <kira_pavlenko_13@mail.ru>
2023-02-10 22:42:54 +0300

    Second commit

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/my2i5us6pig9fi2o?svg=true)](https://ci.appveyor.com/project/KiraMatsenko/selenide)
---
+++ src/test/java/HappyPathTest.java
@@ -0,0 +1,50 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class HappyPathTest {
+
+    @Test
+    void shouldSendSimpleSurname() {
+        open("http://localhost:9999/");
+        $("span[data-test-id=name] input").setValue("Николай Гоголь");
+        $("span[data-test-id=phone] input").setValue("+79211712862");
+        $("label[data-test-id=agreement]").click();
+        $("button[type=button").click();
+        $("p[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test
+    void shouldSendComplexSurname() {
+        open("http://localhost:9999/");
+        $("span[data-test-id=name] input").setValue("Кузьма Петров-Водкин");
+        $("span[data-test-id=phone] input").setValue("+79111111111");
+        $("label[data-test-id=agreement]").click();
+        $("button[type=button").click();
+        $("p[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test
+    void shouldSendYoName() {
+        open("http://localhost:9999/");
+        $("span[data-test-id=name] input").setValue("Ли Вэй");
+        $("span[data-test-id=phone] input").setValue("+71234567890");
+        $("label[data-test-id=agreement]").click();
+        $("button[type=button").click();
+        $("p[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test
+    void shouldSendLongSurname() {
+        open("http://localhost:9999/");
+        $("span[data-test-id=name] input").setValue("Дженифер Аийильцикликирмибайрактазийанкаграманоглу");
+        $("span[data-test-id=phone] input").setValue("+74444444444");
+        $("label[data-test-id=agreement]").click();
+        $("button[type=button").click();
+        $("p[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+}
