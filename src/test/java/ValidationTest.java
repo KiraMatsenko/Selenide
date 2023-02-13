bf426e0268542661d2c770b1dd7b6fa82300b2c6
Kira <kira_pavlenko_13@mail.ru>
2023-02-13 18:40:56 +0300

    Sixth commit

--- src/test/java/ValidationTest.java
+++ src/test/java/ValidationTest.java
@@ -12,7 +12,7 @@ public class ValidationTest {
         $("span[data-test-id=name] input").setValue("Николай Гоголь");
         $("label[data-test-id=agreement]").click();
         $("button[type=button").click();
-        $("div:nth-child(2) > span > span > span.input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+        $("span[data-test-id=phone] > span > span.input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
     }
 
     @Test
@@ -21,7 +21,7 @@ public class ValidationTest {
         $("span[data-test-id=phone] input").setValue("+79111111111");
         $("label[data-test-id=agreement]").click();
         $("button[type=button").click();
-        $("div:nth-child(1) > span > span > span.input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+        $("span[data-test-id=name] > span > span.input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
     }
 
     @Test
@@ -31,7 +31,7 @@ public class ValidationTest {
         $("span[data-test-id=phone] input").setValue("+79111111111");
         $("label[data-test-id=agreement]").click();
         $("button[type=button").click();
-        $("div:nth-child(1) > span > span > span.input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $("span[data-test-id=name] > span > span.input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     @Test
@@ -41,7 +41,7 @@ public class ValidationTest {
         $("span[data-test-id=phone] input").setValue("+7911111111");
         $("label[data-test-id=agreement]").click();
         $("button[type=button").click();
-        $("div:nth-child(2) > span > span > span.input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        $("span[data-test-id=phone] > span > span.input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 
     @Test
@@ -50,6 +50,6 @@ public class ValidationTest {
         $("span[data-test-id=name] input").setValue("Николай Гоголь");
         $("span[data-test-id=phone] input").setValue("+79111111111");
         $("button[type=button").click();
-        $("div:nth-child(2) > span > span > span.input__sub").shouldHave(exactText("На указанный номер моб. тел. будет отправлен смс-код для подтверждения заявки на карту. Проверьте, что номер ваш и введен корректно."));
+        $("span[data-test-id=phone] > span > span.input__sub").shouldHave(exactText("На указанный номер моб. тел. будет отправлен смс-код для подтверждения заявки на карту. Проверьте, что номер ваш и введен корректно."));
     }
 }
