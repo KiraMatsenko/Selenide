298944fafdc2839325481ffddc3094da8fcfd1fa
Kira <kira_pavlenko_13@mail.ru>
2023-02-17 20:53:24 +0300

    Seventh commit

--- src/test/java/ValidationTest.java
+++ src/test/java/ValidationTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -12,7 +13,7 @@ public class ValidationTest {
         $("span[data-test-id=name] input").setValue("Николай Гоголь");
         $("label[data-test-id=agreement]").click();
         $("button[type=button").click();
-        $("span[data-test-id=phone] > span > span.input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+        $("[data-test-id='phone'].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
     }
 
     @Test
@@ -21,7 +22,7 @@ public class ValidationTest {
         $("span[data-test-id=phone] input").setValue("+79111111111");
         $("label[data-test-id=agreement]").click();
         $("button[type=button").click();
-        $("span[data-test-id=name] > span > span.input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
     }
 
     @Test
@@ -31,7 +32,7 @@ public class ValidationTest {
         $("span[data-test-id=phone] input").setValue("+79111111111");
         $("label[data-test-id=agreement]").click();
         $("button[type=button").click();
-        $("span[data-test-id=name] > span > span.input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     @Test
@@ -41,7 +42,7 @@ public class ValidationTest {
         $("span[data-test-id=phone] input").setValue("+7911111111");
         $("label[data-test-id=agreement]").click();
         $("button[type=button").click();
-        $("span[data-test-id=phone] > span > span.input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        $("[data-test-id='phone'].input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 
     @Test
@@ -50,6 +51,6 @@ public class ValidationTest {
         $("span[data-test-id=name] input").setValue("Николай Гоголь");
         $("span[data-test-id=phone] input").setValue("+79111111111");
         $("button[type=button").click();
-        $("span[data-test-id=phone] > span > span.input__sub").shouldHave(exactText("На указанный номер моб. тел. будет отправлен смс-код для подтверждения заявки на карту. Проверьте, что номер ваш и введен корректно."));
+        $("[data-test-id='agreement'].input_invalid").shouldBe(visible);
     }
 }
